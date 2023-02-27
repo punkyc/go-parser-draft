07dda5a928335a3f865b72102bd336a0f97b3ca4
punkyc <cccying520@163.com>
2023-02-27 15:54:12 +0800

    eval function
    1) add object.Function
    2) Eval function handle *ast.FunctionLiteral and *CallExpression
    3) extend function env s.t. support high-order functions and closures

--- src/monkey/object/object.go
+++ src/monkey/object/object.go
@@ -4,6 +4,9 @@ package object
 
 import (
 	"fmt"
+	"bytes"
+	"monkey/ast"
+	"strings"
 )
 
 type ObjectType string
@@ -14,6 +17,8 @@ const (
 	NULL_OBJ = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ = "ERROR"
+
+	FUNCTION_OBJ = "FUNCTION"
 )
 
 type Error struct {
@@ -66,4 +71,30 @@ func (rv *ReturnValue) Type() ObjectType {
 }
 func (rv *ReturnValue) Inspect() string {
 	return rv.Value.Inspect()
+}
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body *ast.BlockStatement
+	Env *Environment
+}
+func (f *Function) Type() ObjectType {
+	return FUNCTION_OBJ
+}
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+	params := []string{}
+
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
 }
